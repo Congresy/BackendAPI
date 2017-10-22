c31bbab9e93b0cc850e577e6ff9dbd7994f0c42f
José María Prieto Gallardo <josprigal@gmail.com>
2017-10-22 13:41:45 +0200

    Nueva carga inicial del proyecto base

---
+++ src/main/java/tfg/conferencias/gestionconferencias/GestionConferenciasApplication.java
@@ -0,0 +1,12 @@
+package tfg.conferencias.gestionconferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionConferenciasApplication.class, args);
+	}
+}
