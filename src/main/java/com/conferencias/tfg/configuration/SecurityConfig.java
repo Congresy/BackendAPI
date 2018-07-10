f068e33c8493be8030f7d9aafd523da5fdfaa6cf
josprigal <josprigal@gmail.com>
2018-07-10 18:48:12 +0200

    Security changes
    Added more changes for cors issues

--- src/main/java/com/conferencias/tfg/configuration/SecurityConfig.java
+++ src/main/java/com/conferencias/tfg/configuration/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
 		http.cors();
 		http.authorizeRequests().antMatchers(HttpMethod.OPTIONS,"/**").permitAll().antMatchers("/actors/all").permitAll().antMatchers("/conference/**").permitAll()
 				.antMatchers("/**").permitAll()
