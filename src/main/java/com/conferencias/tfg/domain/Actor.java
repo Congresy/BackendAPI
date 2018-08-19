502d6aa44018f66854d5417b6a84b2f12b7302b1
andferale <andferale@alum.us.es>
2018-08-19 21:40:44 +0200

    Reestablecido regex para telefono mientras este activo el periodo de pruebas

--- src/main/java/com/conferencias/tfg/domain/Actor.java
+++ src/main/java/com/conferencias/tfg/domain/Actor.java
@@ -26,7 +26,8 @@ public class Actor {
     @NotBlank
     private String email;
     @NotBlank
-    @Pattern(regexp = "^(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\\s*[)]?[-\\s\\.]?[(]?[0-9]{1,3}[)]?([-\\s\\.]?[0-9]{3})([-\\s\\.]?[0-9]{3,4})$")
+    //@Pattern(regexp = "^(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\\s*[)]?[-\\s\\.]?[(]?[0-9]{1,3}[)]?([-\\s\\.]?[0-9]{3})([-\\s\\.]?[0-9]{3,4})$")
+    @Pattern(regexp = "^(\\d{9}|\\d{14})$")
     private String phone;
     @URL
     private String photo;
