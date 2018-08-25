63a88a454deced363842960c9ffbb33e024e6a5c
andferale <andferale@alum.us.es>
2018-08-25 13:19:43 +0200

    Añadido método de pago (test)

--- src/main/java/com/conferencias/tfg/GestionConferenciasApp.java
+++ src/main/java/com/conferencias/tfg/GestionConferenciasApp.java
@@ -1,16 +1,34 @@
 package com.conferencias.tfg;
 
+import com.braintreegateway.BraintreeGateway;
+import com.conferencias.tfg.utilities.BraintreeGatewayFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.File;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableMongoRepositories
 public class GestionConferenciasApp {
 
+	public static String DEFAULT_CONFIG_FILENAME = "config.properties";
+	public static BraintreeGateway gateway;
+
 	public static void main(String[] args) {
+		File configFile = new File(DEFAULT_CONFIG_FILENAME);
+		try {
+			if(configFile.exists() && !configFile.isDirectory()) {
+				gateway = BraintreeGatewayFactory.fromConfigFile(configFile);
+			} else {
+				gateway = BraintreeGatewayFactory.fromConfigMapping(System.getenv());
+			}
+		} catch (NullPointerException e) {
+			System.err.println("Could not load Braintree configuration from config file or system environment.");
+			System.exit(1);
+		}
 
 		SpringApplication.run(GestionConferenciasApp.class, args);
 	}
---
+++ src/main/java/com/conferencias/tfg/controller/CheckoutController.java
@@ -0,0 +1,102 @@
+package com.conferencias.tfg.controller;
+
+import com.braintreegateway.*;
+import com.braintreegateway.Transaction.Status;
+import com.conferencias.tfg.GestionConferenciasApp;
+import io.swagger.annotations.Api;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@CrossOrigin
+@RestController
+@RequestMapping("payments")
+@Api(value = "Payments", description = "Operations related with payments")
+public class CheckoutController {
+
+    private BraintreeGateway gateway = GestionConferenciasApp.gateway;
+
+    private Status[] TRANSACTION_SUCCESS_STATUSES = new Status[] {
+            Transaction.Status.AUTHORIZED,
+            Transaction.Status.AUTHORIZING,
+            Transaction.Status.SETTLED,
+            Transaction.Status.SETTLEMENT_CONFIRMED,
+            Transaction.Status.SETTLEMENT_PENDING,
+            Transaction.Status.SETTLING,
+            Transaction.Status.SUBMITTED_FOR_SETTLEMENT
+    };
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String root(Model model) {
+        return "redirect:checkouts";
+    }
+
+    @RequestMapping(value = "/checkouts", method = RequestMethod.GET)
+    public String checkout(Model model) {
+        String clientToken = gateway.clientToken().generate();
+        model.addAttribute("clientToken", clientToken);
+
+        return "checkouts/new";
+    }
+
+    @RequestMapping(value = "/checkouts", method = RequestMethod.POST)
+    public String postForm(@RequestParam("amount") String amount, @RequestParam("payment_method_nonce") String nonce, Model model, final RedirectAttributes redirectAttributes) {
+        BigDecimal decimalAmount;
+        try {
+            decimalAmount = new BigDecimal(amount);
+        } catch (NumberFormatException e) {
+            redirectAttributes.addFlashAttribute("errorDetails", "Error: 81503: Amount is an invalid format.");
+            return "redirect:checkouts";
+        }
+
+        TransactionRequest request = new TransactionRequest()
+                .amount(decimalAmount)
+                .paymentMethodNonce(nonce)
+                .options()
+                .submitForSettlement(true)
+                .done();
+
+        Result<Transaction> result = gateway.transaction().sale(request);
+
+        if (result.isSuccess()) {
+            Transaction transaction = result.getTarget();
+            return "redirect:checkouts/" + transaction.getId();
+        } else if (result.getTransaction() != null) {
+            Transaction transaction = result.getTransaction();
+            return "redirect:checkouts/" + transaction.getId();
+        } else {
+            String errorString = "";
+            for (ValidationError error : result.getErrors().getAllDeepValidationErrors()) {
+                errorString += "Error: " + error.getCode() + ": " + error.getMessage() + "\n";
+            }
+            redirectAttributes.addFlashAttribute("errorDetails", errorString);
+            return "redirect:checkouts";
+        }
+    }
+
+    @RequestMapping(value = "/checkouts/{transactionId}")
+    public String getTransaction(@PathVariable String transactionId, Model model) {
+        Transaction transaction;
+        CreditCard creditCard;
+        Customer customer;
+
+        try {
+            transaction = gateway.transaction().find(transactionId);
+            creditCard = transaction.getCreditCard();
+            customer = transaction.getCustomer();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            return "redirect:/checkouts";
+        }
+
+        model.addAttribute("isSuccess", Arrays.asList(TRANSACTION_SUCCESS_STATUSES).contains(transaction.getStatus()));
+        model.addAttribute("transaction", transaction);
+        model.addAttribute("creditCard", creditCard);
+        model.addAttribute("customer", customer);
+
+        return "checkouts/show";
+    }
+}
